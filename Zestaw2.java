81a3d637fa92939322b5e064b8cdf1a21e0f3693
gabost1023 <161207635+gabost1023@users.noreply.github.com>
2024-10-12 17:07:59 +0200

    Update Zestaw2.java

--- Zestaw2.java
+++ Zestaw2.java
@@ -1,1 +1,34 @@
+public class Zestaw2 {
 
+    public static void main(String[] args) {
+
+        //cw1
+        double liczba1 = 0.23 + 76;
+        System.out.println(liczba1);
+        double liczba2 = 41*2.0 + 3;
+        System.out.println(liczba2);
+        int liczba3 = 5 - 33;
+        System.out.println(liczba3);
+        int liczba4 = 109%3;
+        System.out.println(liczba4);
+        int liczba5 = 50/2;
+        System.out.println(liczba5);
+        int liczba6 = 4|2;
+        System.out.println(liczba6);
+        double liczba7 = Math.pow(3, 5);
+        System.out.println(liczba7);
+        int liczba8 = 7 & 9;
+        System.out.println(liczba8);
+
+        //cw2
+        // a)
+        double gora = Math.pow(7,(1/2)) - 1;
+        double calosc = (gora/2) + (Math.pow(3,3)%2);
+        System.out.println(calosc);
+        int zrzutuj = (int)calosc;
+        System.out.println(zrzutuj);
+
+
+
+    }
+}
